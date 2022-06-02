658765f3400a05014692ac6cdd557aa1d221c49e
Brian de Reus <briandereus69@gmail.com>
2022-06-02 10:26:35 +0200

    ShootingMechanic

---
+++ Space/Assets/BulletR2.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletR2 : MonoBehaviour
+{
+    public float speed = 7f;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }
+}
